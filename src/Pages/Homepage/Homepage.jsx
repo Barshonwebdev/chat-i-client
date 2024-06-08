85d17bb9e6ecc40948f03e972d136bf7911d8995
Barshon <barshonweb@gmail.com>
2024-06-09 02:12:58 +0600

    chatbox component

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,24 +1,10 @@
-import { AiFillCaretRight } from "react-icons/ai";
+import Chatbox from "../../components/Chatbox";
 
 
 const Homepage = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <div className="card w-96 h-72 bg-base-100 shadow-2xl">
-       
-        <div className="card-body flex-col justify-between">
-          <h2 className="card-title mx-auto text-2xl italic font-bold text-blue-900">Chatbox</h2>
-          <div className="card-actions block ">
-           
-            <label className="block italic mb-2">Message: </label>
-           <div className="flex items-center">
-           <input placeholder="Aa" type="text" name="" id="" className="p-1 px-3 w-full  border-base-300 border rounded-xl" /> 
-          <button> <AiFillCaretRight className="ms-2 text-2xl text-green-950"></AiFillCaretRight></button>
-           </div>
-           
-          </div>
-        </div>
-      </div>
+     <Chatbox/>
     </div>
   );
 };
---
+++ src/components/Chatbox.jsx
@@ -0,0 +1,25 @@
+import { AiFillCaretRight } from "react-icons/ai";
+
+const Chatbox = () => {
+    return (
+        <div>
+             <div className="card w-96 h-72 bg-base-100 shadow-2xl">
+       
+       <div className="card-body flex-col justify-between">
+         <h2 className="card-title mx-auto text-2xl italic font-bold text-blue-900">Chatbox</h2>
+         <div className="card-actions block ">
+          
+           <label className="block italic mb-2">Message: </label>
+          <div className="flex items-center">
+          <input placeholder="Aa" type="text" name="" id="" className="p-1 px-3 w-full  border-base-300 border rounded-xl" /> 
+         <button> <AiFillCaretRight className="ms-2 text-2xl text-green-950"></AiFillCaretRight></button>
+          </div>
+          
+         </div>
+       </div>
+     </div>
+        </div>
+    );
+};
+
+export default Chatbox;
