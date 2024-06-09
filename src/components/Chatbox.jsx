9549636039f10ef0764160bf9dc4fd66f1afb2b5
Barshon <barshonweb@gmail.com>
2024-06-09 19:34:33 +0600

    chat app overflow fix

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { AiFillCaretRight } from "react-icons/ai";
 import socketIOClient from 'socket.io-client';
 
@@ -28,39 +28,41 @@ useEffect(()=>{
    return socket.disconnect();
   };
 
-},[])
+},[chats])
 
 const sendChat=()=>{
   const socket=socketIOClient(ENDPOINT);
   socket.emit('message',chat);
+ 
   setChat('');
   console.log(chats);
 }
 
     return (
         <div>
-             <div className="card w-96 h-72 bg-base-100 shadow-2xl">
+             <div className="card w-96 h-96 bg-base-100 shadow-2xl">
        
-       <div className="card-body">
+       <div className="card-body flex-col justify-between">
          <h2 className="card-title mx-auto text-2xl italic font-bold text-blue-900">Chatbox</h2>
-         <div>
+        <div>
+        <div  className="border-base-300 border h-52 rounded-xl p-1 px-3 overflow-auto">
           {
             chats.map((msg,idx)=>(
-              <div key={idx}>
-                <p>{msg.chat}</p>
+              <div  className="" key={idx}>
+                <p className="my-2">{msg.chat}</p>
               </div>
             ))
           }
          </div>
-         <div className="card-actions block ">
+         <div className="card-actions block mt-7 ">
           
-           <label className="block italic mb-2">Message: </label>
           <div className="flex items-center">
           <input value={chat} onChange={(e)=>setChat(e.target.value)} placeholder="Aa" type="text" name="" id="" className="p-1 px-3 w-full  border-base-300 border rounded-xl" /> 
          <button onClick={sendChat}> <AiFillCaretRight className="ms-2 text-2xl text-green-950"></AiFillCaretRight></button>
           </div>
           
          </div>
+        </div>
        </div>
      </div>
         </div>
